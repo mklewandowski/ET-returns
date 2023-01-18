78e0c8f71f1120de747c9f7f84dc3bf957d29540
mklewandowski <matt.kain.lewandowski@gmail.com>
2023-01-17 23:34:35 -0800

    Change enemy so seek position updates on timer

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    Transform playerTransform;
+
     bool isActive = true;
+
+    private Rigidbody2D enemyRigidbody;
+    float moveSpeed = .1f;
+    Vector2 movementVector = new Vector2(0, 0);
+
+    float positionTimer = .1f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        playerTransform = GameObject.Find("Player").transform;
+        enemyRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (positionTimer > 0)
+        {
+            positionTimer -= Time.deltaTime;
+            if (positionTimer < 0)
+            {
+                movementVector = playerTransform.position - this.transform.localPosition;
+                movementVector = movementVector * moveSpeed;
+                positionTimer = Random.Range(.2f, .5f);
+            }
+        }
+        enemyRigidbody.velocity = movementVector;
     }
 
     void OnTriggerEnter2D(Collider2D collider)
