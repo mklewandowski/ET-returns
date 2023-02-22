6572f9072578c137e6a14383d55ffe3245540312
mklewandowski <matt.kain.lewandowski@gmail.com>
2023-02-22 14:30:38 -0800

    Improve random color selection

--- Assets/Scripts/Candy.cs
+++ Assets/Scripts/Candy.cs
@@ -6,14 +6,13 @@ public class Candy : MonoBehaviour
 {
     public void Init()
     {
-        float randVal = Random.Range(0, 100f);
-        Color newColor = Color.white;
-        if (randVal < 33f)
-            newColor = new Color(255f/255f, 56f/255f, 1f/255f); // orange
-        else if (randVal < 66f)
-            newColor = new Color(251f/255f, 238f/255f, 1f/255f); // yellow
-        else
-            newColor = new Color(82f/255f, 24f/255f, 0f/255f); // brown
+        Color[] candyColors = new Color[]
+        {
+            new Color(255f/255f, 56f/255f, 1f/255f), // orange
+            new Color(251f/255f, 238f/255f, 1f/255f), // yellow
+            new Color(82f/255f, 24f/255f, 0f/255f) // brown
+        };
+        Color newColor = candyColors[Random.Range(0, candyColors.Length)];
         this.GetComponent<SpriteRenderer>().color = newColor;
     }
 }
