dfec6d87cfd08f2fb4ae9a36403fb49fa69959c6
mklewandowski <matt.kain.lewandowski@gmail.com>
2023-06-07 10:08:30 -0700

    Fix tornado index bug

--- Assets/Scripts/Tornado.cs
+++ Assets/Scripts/Tornado.cs
@@ -6,7 +6,7 @@ public class Tornado : MonoBehaviour
 {
     public void Init(Vector3 centerPos)
     {
-        int index = (int)Globals.UpgradeTypes.Bees * Globals.MaxLevelsPerUpgrade + Globals.CurrentUpgradeLevels[(int)Globals.UpgradeTypes.Bees] - 1;
+        int index = (int)Globals.UpgradeTypes.Tornado * Globals.MaxLevelsPerUpgrade + Globals.CurrentUpgradeLevels[(int)Globals.UpgradeTypes.Tornado] - 1;
         int enemyHitsAllowed = Globals.UpgradeLevelEnemyHits[index];
         this.GetComponent<Bullet>().SetEnemyHits(enemyHitsAllowed);
         this.transform.localPosition = new Vector2(centerPos.x + Random.Range(9.2f, 9.7f), Random.Range(centerPos.y - 3f, centerPos.y + 3f));
